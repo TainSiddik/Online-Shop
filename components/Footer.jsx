ed06c80098778db2181919a73afb011ccb78b3f5
Ta'in Siddik <tainsiddik11@gmail.com>
2025-10-09 01:18:47 +0700

    create header and products components

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,17 +1,17 @@
-import Image from "next/image";
-import { MdOutlineWhatsapp } from "react-icons/md";
-import { IoLogoInstagram, IoLogoTiktok } from "react-icons/io5";
-import { FaTelegram } from "react-icons/fa";
-import Link from "next/link";
+import Image from 'next/image';
+import { MdOutlineWhatsapp } from 'react-icons/md';
+import { IoLogoInstagram, IoLogoTiktok } from 'react-icons/io5';
+import { FaTelegram } from 'react-icons/fa';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <footer>
-      <div className="footer bg-slate-500 px-6 py-4 flex items-center">
+      <div className="footer bg-slate-300 mt-12 px-6 py-1 flex items-center">
         <div className="logo mx-auto">
-          <Image src="/logo.png" width={100} height={100} alt="logo" />
+          <Image src="/logo.png" width={150} height={100} alt="logo" />
         </div>
-        <div className="contact mx-auto text-white">
+        <div className="contact mx-auto text-slate-700">
           <div className="icon flex justify-center gap-12">
             <Link href="/">
               <MdOutlineWhatsapp />
@@ -26,8 +26,8 @@ const Footer = () => {
               <FaTelegram />
             </Link>
           </div>
-          <div className="menu mt-6">
-            <ul className="flex space-x-12 text-white">
+          <div className="menu mt-4">
+            <ul className="flex space-x-12 text-slate-700">
               <li>
                 <Link href="/">Home</Link>
               </li>
